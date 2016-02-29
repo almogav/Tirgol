942e86cc9c13bd3f5a9f8423accda376ea42cace
Student <Student@10.0.11.6>
2016-02-29 17:41:42 +0200

    one

---
+++ tirgol/src/tirgol/aaa.java
@@ -0,0 +1,11 @@
+package tirgol;
+
+public class aaa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+
+	}
+
+}
---
+++ tirgol/src/tirgol/smartStu.java
@@ -0,0 +1,5 @@
+package tirgol;
+
+public class smartStu {
+
+}
